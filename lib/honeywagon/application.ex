24a4f83cdf399b32503151fd8d3a704facb20f8f
Minoru Maekawa <126235344+err931@users.noreply.github.com>
2026-09-03 09:10:45 +0900

    chore: add skeleton app
    Signed-off-by: Minoru Maekawa <126235344+err931@users.noreply.github.com>

---
+++ lib/honeywagon/application.ex
@@ -0,0 +1,20 @@
+defmodule Honeywagon.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Honeywagon.Worker.start_link(arg)
+      # {Honeywagon.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Honeywagon.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
